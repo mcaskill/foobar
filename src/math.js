fd4cfb0e1da57bcc72b4409e0fbfc8e6d43edff8
Chauncey McAskill <chauncey@mcaskill.ca>
2021-06-06 12:18:15 -0400

    Complexify codebase
    To increase coverage metrics.

--- src/math.js
+++ src/math.js
@@ -1,3 +1,35 @@
-export const sum = (a, b) => a + b;
-export const div = (a, b) => a / b;
-export const mod = (a, b) => a % b;
+export const sum = (a, b) => {
+  if (typeof a !== 'number') {
+    throw new TypeError('First parameter must be a number');
+  }
+
+  if (typeof b !== 'number') {
+    throw new TypeError('Second parameter must be a number');
+  }
+
+  return a + b;
+};
+
+export const div = (a, b) => {
+  if (typeof a !== 'number') {
+    throw new TypeError('First parameter must be a number');
+  }
+
+  if (typeof b !== 'number') {
+    throw new TypeError('Second parameter must be a number');
+  }
+
+  return a / b;
+};
+
+export const mod = (a, b) => {
+  if (typeof a !== 'number') {
+    throw new TypeError('First parameter must be a number');
+  }
+
+  if (typeof b !== 'number') {
+    throw new TypeError('Second parameter must be a number');
+  }
+
+  return a % b;
+};
